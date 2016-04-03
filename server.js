fa764e8b947324b786960af476f8f141789dcae8
Peter Gyory <ptg3662@rit.edu>
2016-04-03 02:33:58 -0400

    Install missing loaders

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var config = require('./webpack.config')
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var config = require('./webpack.config');
 
-var app = new (require('express'))()
-var PORT = 3000
+var app = new (require('express'))();
+var PORT = 3000;
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+var compiler = webpack(config);
+// app.use(webpackDevMiddleware(compiler,
+// { noInfo: true, publicPath: config.output.publicPath }));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
-})
+});
 
 app.listen(PORT, function(error) {
   if (error) {
-    console.error(error)
+    console.error(error);
   } else {
-    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, PORT)
+    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', PORT, PORT);
   }
-})
+});
