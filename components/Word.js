82fcfa93fbb560b02fa1bf871358e0763c3f6d7b
Peter Gyory <ptg3662@rit.edu>
2016-05-09 18:22:39 -0400

    Add word component

---
+++ components/Word.js
@@ -0,0 +1,15 @@
+import React, { PropTypes }  from 'react';
+
+const propTypes = {
+  selected: PropTypes.bool.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+const Word = ({ selected, content }) =>
+  <span
+  className={`word ${selected ? 'selected' : 'unselected'}`}
+  >{content}</span>;
+
+
+Word.propTypes = propTypes;
+export default Word;
--- containers/App.js
+++ containers/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-export default () => ( <div>
-  <h1>Stuff</h1>
-</div> );
+export default () =>
+  <div>
+    <h1>Stuff</h1>
+  </div>;
