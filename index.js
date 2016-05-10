6378aa117e9123ccbe05ca9b884893149e125f14
Will <whp3652@rit.edu>
2016-05-10 14:41:51 -0400

    Move everything to a src/ dir

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import Editor from './containers/Editor';
-import reducers from './reducers';
+import Editor from './src/containers/Editor';
+import reducers from './src/reducers';
 
-import dubliners from './texts/dubliners';
-import parser from './parser/parser';
+import dubliners from './src/texts/dubliners';
+import parser from './src/parser/parser';
 
-console.log('this works')
+console.log('this works');
 console.log(parser.tokenize(dubliners));
 
 const store = createStore(reducers);
