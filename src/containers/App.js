67cccb114a2254f45d893e863a66def7a9a8d580
Peter Gyory <ptg3662@rit.edu>
2016-05-11 23:24:34 -0400

    Use function shorthand

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,24 +10,20 @@ const tempWords = ['peter', 'peter', 'peter', 'peter', 'peter', 'peter', 'peter'
 const tempFilter = [true, true, true, true, true, true, true, true, true, true, true, true, true];
 
 const App = React.createClass({
-  clickMe: function () {
-    const that = this;
-    this.setState({ filter: tempAltFilter }, () => console.log(that.state));
-  },
-
   getInitialState: () =>
     ({
       words: tempWords,
       filter: tempFilter,
     }),
 
-  render: function () {
-    return (<div className="peter" onClick={this.clickMe}>
-      <Editor />
-      <TextViewer
-        words={this.state.words}
-        filter={this.state.filter} />
-    </div>);
+  render() {
+    return (
+      <div>
+        <Editor />
+        <TextViewer
+          words={this.state.words}
+          filter={this.state.filter} />
+      </div>);
   },
 });
 
