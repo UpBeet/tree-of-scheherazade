d94a8bfcb0e413fcc2ce4b0bdbc29768b84974c8
Peter Gyory <ptg3662@rit.edu>
2016-05-12 00:44:23 -0400

    Newline chars display as block

--- src/components/Word.js
+++ src/components/Word.js
@@ -3,8 +3,12 @@ import React, { PropTypes } from 'react';
 const getClass = (highlight, selected) =>
   `word ${selected ? '' : 'unselected'} ${highlight ? 'highlight' : ''}`;
 
+const isNewline = string => string === '\n';
+
 const Word = ({ highlight, selected, content }) =>
-  (<span className={getClass(highlight, selected)}>
+  (<span
+    className={getClass(highlight, selected)}
+    style={{ display: isNewline(content) ? 'block' : 'inline-block' }}>
         {content}
       </span>);
 
